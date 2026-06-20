91b991611edd40538c07b937dec94e4420ab24cd
JEERYEX <165129000+JEERYEX@users.noreply.github.com>
2026-06-20 09:14:06 +0500

    Update 0_SHAAN-OUT-ALL.js

--- models/commands/0_SHAAN-OUT-ALL.js
+++ models/commands/0_SHAAN-OUT-ALL.js
@@ -2,7 +2,7 @@ module.exports.config = {
         name: "outall",
         version: "1.0.0",
         hasPermssion: 2,
-        credits: "SHAAN KHAN ",
+        credits: "JERRY EX ",
         description: "Bot leaves all groups except the current one",
         commandCategory: "Admin",
         usages: "outall",
@@ -14,7 +14,7 @@ module.exports.run = async ({ api, event, args }) => {
     const permission = ["100016828397863", "", ""];
     
     if (!permission.includes(event.senderID))
-        return api.sendMessage("Sorry boss, mujhe sirf mere Shaan Khan hi saare groups se leave karwa sakte hain 🙂✌️", event.threadID, event.messageID);
+        return api.sendMessage("Sorry boss, mujhe sirf mere Jerry ex hi saare groups se leave karwa sakte hain 🙂✌️", event.threadID, event.messageID);
 
     return api.getThreadList(100, null, ["INBOX"], (err, list) => {
         if (err) {
@@ -28,6 +28,6 @@ module.exports.run = async ({ api, event, args }) => {
             }
         });
 
-        api.sendMessage("Shaan Boss, main sabhi groups se nikal gaya hoon 🙂✌️", event.threadID);
+        api.sendMessage("Jerry Boss, main sabhi groups se nikal gaya hoon 🙂✌️", event.threadID);
     });
 };
