1c2363f6ea24a523ccd4da9c17e2e077e79f1fb8
JEERYEX <165129000+JEERYEX@users.noreply.github.com>
2026-06-20 08:38:19 +0500

    Update files.js

--- models/commands/files.js
+++ models/commands/files.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "file",
     version: "1.0.1",
     hasPermssion: 2,
-    credits: "SHAAN",
+    credits: "JERRY",
     description: "Delete the file or folder in the commands folder",
     commandCategory: "Admin",
     usages: "\ncommands start <text>\ncommands ext <text>\ncommands <text>\ncommands [leave blank]\ncommands help\nNOTE: <text> is the character you want to enter",
