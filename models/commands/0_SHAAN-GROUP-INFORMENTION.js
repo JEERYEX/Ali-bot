2872187d20db23ee06ed6e6b7d41c8fafce61ee8
JEERYEX <165129000+JEERYEX@users.noreply.github.com>
2026-06-20 09:20:41 +0500

    Update 0_SHAAN-GROUP-INFORMENTION.js

--- models/commands/0_SHAAN-GROUP-INFORMENTION.js
+++ models/commands/0_SHAAN-GROUP-INFORMENTION.js
@@ -5,7 +5,7 @@ module.exports.config = {
 	version: "1.0.0", 
 	hasPermssion: 1,
 	credits: "JERRY BABU",
-	description: "THIS BOT WAS MADE BY MR SHAAN BABU",
+	description: "THIS BOT WAS MADE BY MR JERRY BABU",
 	commandCategory: "GROUP INFORMETION", 
 	usages: "PREFIX", 
 	cooldowns: 0,
