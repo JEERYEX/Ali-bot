cf9413b189513154dfd91c4b60e8d6d6b25442c9
JEERYEX <165129000+JEERYEX@users.noreply.github.com>
2026-06-20 08:39:07 +0500

    Update busy.js

--- models/commands/busy.js
+++ models/commands/busy.js
@@ -2,7 +2,7 @@ module.exports.config = {
         name: "busy",
         version: "1.0.0",
         permissions: 1,
-        credits: "Mr Shaan",
+        credits: "Jerry Ex",
         description: "Turn busy mode on or off",
   usages: "[reason]",
   commandCategory: "Tiện ích",
