cfee0956f016534a69ff1db66b46dceeb3aeb722
JEERYEX <165129000+JEERYEX@users.noreply.github.com>
2026-06-20 09:04:29 +0500

    Update 0_SHAAN-SAY.js

--- models/commands/0_SHAAN-SAY.js
+++ models/commands/0_SHAAN-SAY.js
@@ -2,7 +2,7 @@ module.exports.config = {
         name: "say",
         version: "1.0.2",
         hasPermssion: 0,
-        credits: "Shaan",
+        credits: "Jerry",
         description: "check the amount of yourself or the person tagged",
         commandCategory: "without prefix",
         usages: "[tag]",
